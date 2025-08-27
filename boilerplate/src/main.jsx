3c0b672001b2be2f1f8dc8ea8da8bdd260dcc9da
andre-49 <andy49miguel@gmail.com>
2025-08-27 19:20:55 +0200

    boilerplare setup complete

--- boilerplate/src/main.jsx
+++ boilerplate/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 function App() {
-  return <div className='text-blue-900 text-5xl'>Boilerplate;</div>
+  return <div className='text-blue-900 text-5xl text-center'>Boilerplate;</div>
 }
 
 createRoot(document.getElementById('root')).render(
